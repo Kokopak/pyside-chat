05cb129e42e6105ceace2537605bf2b354c8c4a2
Chataignon Corentin <corentin.chataignon44@gmail.com>
2014-05-05 10:40:27 +0200

    Creation branche dev

--- server-console.py
+++ server-console.py
@@ -8,44 +8,25 @@ from PySide import QtCore, QtNetwork
 class Server(QtNetwork.QTcpServer):
     def __init__(self, parent=None):
         super(Server, self).__init__(parent)
-
-        self.clients = []
         self.newConnection.connect(self.newClient)
-        self.blockSize = 0
+        self.clients = []
 
     def newClient(self):
         client = self.nextPendingConnection()
-        self.clients.append(client)
         client.readyRead.connect(self.readData)
+        self.clients.append(client)
+        print "Connexion de %s" % client
 
     def readData(self):
         socket = self.sender()
         print "Reçu paquet de {0} :".format(socket)
-
-        instr = QtCore.QDataStream(socket)
-        if self.blockSize == 0:
-            if socket.bytesAvailable() < 2:
-                return
-
-            self.blockSize = instr.readUInt16()
-        if socket.bytesAvailable() < self.blockSize:
-            return
-
-        message = instr.readString()
+        message = socket.readAll()
         print message
         self.sendAll(message)
-        self.blockSize = 0
 
     def sendAll(self, message):
-        block = QtCore.QByteArray()
-        outstr = QtCore.QDataStream(block, QtCore.QIODevice.WriteOnly)
-        outstr.writeUInt16(0)
-        outstr.writeString(message)
-        outstr.device().seek(0)
-        outstr.writeUInt16(block.count() - 2)
-
         for c in self.clients:
-            c.write(block)
+            c.write(message.toUtf8())
 
 if __name__ == '__main__':
 
@@ -56,4 +37,3 @@ if __name__ == '__main__':
     serv.listen(port=port)
     print "Le serveur tourne sur le port %d" % port
     sys.exit(app.exec_())
-
